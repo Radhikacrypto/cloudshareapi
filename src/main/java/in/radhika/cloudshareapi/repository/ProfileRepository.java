2239eac7b3e51bf2accb52f0fdc48f287bd17700
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2026-05-10 02:22:22 +0530

    changes made

--- src/main/java/in/radhika/cloudshareapi/repository/ProfileRepository.java
+++ src/main/java/in/radhika/cloudshareapi/repository/ProfileRepository.java
@@ -1,16 +1,15 @@
 package in.radhika.cloudshareapi.repository;
 
-import java.util.Optional;
-
+import in.radhika.cloudshareapi.document.ProfileDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import in.radhika.cloudshareapi.document.ProfileDocument;
+import java.util.Optional;
 
-public interface ProfileRepository extends MongoRepository<ProfileDocument, String>{
-    
+public interface ProfileRepository extends MongoRepository<ProfileDocument, String> {
+
     Optional<ProfileDocument> findByEmail(String email);
-    Optional<ProfileDocument> findByClerkId(String clerkId);
+
+    ProfileDocument findByClerkId(String clerkId);
 
     Boolean existsByClerkId(String clerkId);
-
 }
